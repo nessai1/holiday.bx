9adaaca4a62cef462be043b0dea7c2163b5713a1
Daniil Verevkin <werowkin@gmail.com>
2021-01-07 12:57:53 +0200

    Добавил функционал в компоратор

--- difference.bx/Compiler.php
+++ difference.bx/Compiler.php
@@ -12,11 +12,36 @@ class Compiler
       // TODO: some manipulation with files
     }
 
+    /**
+     * Function that set all states of file to 'stable'
+     * @param ModifyTextDocument $document link to modify doc
+     * @throws WrongIndexException
+     */
     public static function clearState(ModifyTextDocument &$document) : void
     {
         for ($i = 0; $i < $document->getSize(); $i++)
         {
             $document->setState($i, 'stable');
         }
+    }
+
+    /**
+     * Function that return count of match of 2 strings
+     * @param string $str1 first string
+     * @param string $str2 second string
+     * @return int count of matches $str1 and $str2
+     */
+    public static function findMatches(string $str1, string $str2) : int
+    {
+        $matches = 0;
+        $minSizeOfStrings = min(strlen($str1), strlen($str2));
+        for ($i = 0; $i < $minSizeOfStrings; $i++)
+        {
+            if ($str1[$i] == $str2[$i])
+            {
+                $matches++;
+            }
+        }
+        return $matches;
     }
 }
