488b1ca53565f792c122728464da971810c85958
Daniil Verevkin <werowkin@gmail.com>
2021-01-07 07:05:53 +0200

    Добавил еще тесты для компоратора и файлы с образцами текстов для дальнеших написаниев тестов под эти файлы tmpFiles/unequal*.txt: образец для тестирования

--- tests/cases/CompilerTest.php
+++ tests/cases/CompilerTest.php
@@ -20,7 +20,7 @@ class CompilerTest extends TestCase
         }
     }
 
-    public function testUnequalFiles() : void
+    public function testUnequalFilesCase1() : void
     {
         $beforeLines = ['Hello world', 'Hello PHP'];
         $afterLines = ['Hello world', 'test message', 'Hello PHP'];
@@ -37,5 +37,35 @@ class CompilerTest extends TestCase
         self::assertEquals('add', $secondFile->getState(1));
         self::assertEquals('stable', $secondFile->getState(2));
 
+    }
+
+    public function testUnequalFilesCase2() : void
+    {
+        $beforeLines = [];
+        $afterLines = ['Hello world', 'Hello PHP'];
+        $firstFile = new ModifyTextDocument(new SafeTextDocument(new TextDocument($beforeLines)));
+        $secondFile = new ModifyTextDocument(new SafeTextDocument(new TextDocument($afterLines)));
+        Compiler::compare($firstFile, $secondFile);
+        for ($i = 0; $i < $secondFile->getSize(); $i++)
+        {
+            self::assertEquals('add',$secondFile->getState($i));
+        }
+    }
+
+    public function testUnequalFilesCase3() : void
+    {
+        $beforeLines = ['Love PHP', 'Love World'];
+        $afterLines = ['Hello PHP', 'Hello World', 'Love PHP', 'Love World'];
+        $firstFile = new ModifyTextDocument(new SafeTextDocument(new TextDocument($beforeLines)));
+        $secondFile = new ModifyTextDocument(new SafeTextDocument(new TextDocument($afterLines)));
+        Compiler::compare($firstFile, $secondFile);
+        for ($i = 0; $i < $firstFile->getSize(); $i++)
+        {
+            self::assertEquals('stable', $firstFile->getState($i));
+        }
+        self::assertEquals('add', $secondFile->getState(0));
+        self::assertEquals('add', $secondFile->getState(1));
+        self::assertEquals('stable', $secondFile->getState(2));
+        self::assertEquals('stable', $secondFile->getState(3));
     }
 }
