9fbddd069efab855e5226ce12713b5f571ca5b82
Daniil Verevkin <werowkin@gmail.com>
2021-01-17 23:10:33 +0200

    Создал класс Manipulator и переименовал DataGetter в DataReceiver. Добавил в DataReceiver функцию getAllCompareSessionsInfo получающую список всех сравнений Manipulator.php: класс который просто взаимодействует с классом записи данных и классом чтения данных из БД.

--- difference.bx/Database/DataReceiver.php
+++ difference.bx/Database/DataReceiver.php
@@ -2,10 +2,10 @@
 
 require_once (__DIR__ . '/Database.php');
 require_once (__DIR__ . '/../Document/ModifyTextDocument.php');
-require_once (__DIR__ . '/../Exceptions/DataGetterException.php');
+require_once (__DIR__ . '/../Exceptions/DataReceiverException.php');
 
 
-class DataGetter
+class DataReceiver
 {
     private static self $instance;
     protected Database $database;
@@ -53,7 +53,7 @@ class DataGetter
         try {
             $filesIDs = $this->database->makeQuery("SELECT FIRST_FILE, SECOND_FILE FROM compare WHERE ID = {$sessionID}")->fetch_assoc();
             if ($filesIDs === null) {
-                throw new DataGetterException("comparison with input ID not found");
+                throw new DataReceiverException("comparison with input ID not found");
             }
             $firstTextDoc = new ModifyTextDocument(new TextDocument($this->getFileContentByID($filesIDs['FIRST_FILE'])));
             $secondTextDoc = new ModifyTextDocument(new TextDocument($this->getFileContentByID($filesIDs['SECOND_FILE'])));
@@ -77,18 +77,38 @@ class DataGetter
                 $secondTextDoc->setState($i, $secondFileStates[$i]);
             }
         }
-        catch (DataGetterException $e)
+        catch (DataReceiverException $e)
         {
             $this->logger->log($e->getMessage());
-            throw new DataGetterException("An occurred error while finding compared files");
+            throw new DataReceiverException("An occurred error while finding compared files");
         }
         catch (DatabaseQueryException $e)
         {
             $message = "An occurred database query error while finding compare files: {$e->getMessage()}";
             $this->logger->log($message);
-            throw new DataGetterException($message);
+            throw new DataReceiverException($message);
         }
         return $filesIDs;
+    }
+
+    public function getAllCompareSessionsInfo() : array
+    {
+        try
+        {
+            $query =
+                "SELECT a.ID as ID, a.COMPARE_DATE as COMPARE_DATE, b.FILE_NAME as FIRST_FILE, c.FILE_NAME as SECOND_FILE FROM compare a
+                LEFT JOIN files b  on b.ID = a.FIRST_FILE
+                LEFT JOIN files c on c.ID = a.SECOND_FILE";
+            $queryResult = $this->database->makeQuery($query);
+            $queryResultArray = $queryResult->fetch_all(MYSQLI_ASSOC);
+        }
+        catch (DatabaseQueryException $e)
+        {
+            $message = "An occurred database query error while getting compare sessions info: {$e->getMessage()}";
+            $this->logger->log($message);
+            throw new DataReceiverException($message);
+        }
+        return $queryResultArray;
     }
 
     public static function getInstance(Database $db) : self
--- difference.bx/Exceptions/DataGetterException.php
+++
@@ -1,9 +0,0 @@
-<?php
-
-class DataGetterException extends Exception
-{
-    public function __construct($message, $code = 2)
-    {
-        parent::__construct("[DataGetter] {$message}", $code);
-    }
-}
