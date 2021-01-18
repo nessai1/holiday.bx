c9e3b979b50a1012f2010372f7dcd0248979a195
Daniil Verevkin <werowkin@gmail.com>
2021-01-19 01:31:33 +0200

    Подготовил формы index.php для передачи данных на сервер

--- difference.bx/index.php
+++ difference.bx/index.php
@@ -45,33 +45,33 @@ require_once (__DIR__ . '/FileReader.php');
         <button class="button" id="textButton" onclick="changeTextForm()">Сравнить текст</button>
     </div>
 
-    <form action="" class="loadForm" id="fileMenu" style="display: none">
+    <form method="POST" action="compare.php" class="loadForm" id="fileMenu" enctype="multipart/form-data" style="display: none">
         <div class="loadForm__files">
             <div class="loadForm__file">
             <h3 class="loadForm__title">Before file</h3>
-            <input type="file">
+            <input type="file" name="firstFile" required>
             </div>
             <div class="loadForm__file">
             <h3 class="loadForm__title">After file</h3>
-            <input type="file">
+            <input type="file" name="secondFile" required>
             </div>
         </div>
-        <button type="submit" class="button" style="margin-top: 70px">Сравнить!</button>
+        <button type="submit" class="button" style="margin-top: 70px" name="loadFiles">Сравнить!</button>
     </form>
 
 
-    <form action="" class="loadForm" id="textMenu" style="display: none">
+    <form method="POST" action="compare.php" class="loadForm" id="textMenu" style="display: none">
             <div class="areas">
             <div class="textInput">
-                <input type="text" placeholder="first file name" class="textInput__filename">
-                <textarea placeholder="Your text after" wrap="off" class="textInput__content" type="text"></textarea>
+                <input type="text" placeholder="first file name" class="textInput__filename" value="FirstFile.txt" name="firstName">
+                <textarea placeholder="Your text after" wrap="off" class="textInput__content" type="text" name="firstText"></textarea>
             </div>
             <div class="textInput">
-                <input type="text" placeholder="second file name" class="textInput__filename">
-            <textarea placeholder="Your text before" wrap="off" class="textInput__content" type="text"></textarea>
+                <input type="text" placeholder="second file name" class="textInput__filename" value="SecondFile.txt" name="secondName">
+            <textarea placeholder="Your text before" wrap="off" class="textInput__content" type="text" name="secondText"></textarea>
             </div>
             </div>
-        <button type="submit" class="button loadForm__button" style="flex-direction: row">Сравнить!</button>
+        <button type="submit" class="button loadForm__button" style="flex-direction: row" name="loadTexts">Сравнить!</button>
     </form>
 </div>
 
