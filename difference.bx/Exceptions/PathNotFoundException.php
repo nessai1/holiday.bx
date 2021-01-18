111e3ba71aa0cb1663187dce6f7e3a6820b49070
Daniil Verevkin <werowkin@gmail.com>
2021-01-19 01:32:38 +0200

    Добавил новые описания ошибок в конфиг сервера и выставил стандартные коды исключений в конструкторе для определения ошибок через конфиг

--- difference.bx/Exceptions/PathNotFoundException.php
+++ difference.bx/Exceptions/PathNotFoundException.php
@@ -7,9 +7,9 @@ class PathNotFoundException extends Exception
 
     private string $path;
 
-    public function __construct(string $path)
+    public function __construct(string $path, int $code = 3)
     {
-        parent::__construct('Path '.$path.' is not found');
+        parent::__construct('Path '.$path.' is not found', $code);
         $this->path = $path;
     }
 
