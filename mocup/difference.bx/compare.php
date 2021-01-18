cd0a7f197ad1030873297a431246353cd7d9f6d0
Daniil Verevkin <werowkin@gmail.com>
2021-01-18 15:28:37 +0200

    Сверстал frontend сайта

---
+++ mocup/difference.bx/compare.php
@@ -0,0 +1,98 @@
+<?php
+require_once (__DIR__ . '/header.php');
+?>
+
+    <div class="compareResult">
+        <h1 class="compareResult__title">Результат сравнения</h1>
+
+        <div class="compareResult__tables">
+
+
+            <div class="codeTable">
+                <div class="codeTable__head">
+                    <span>main.txt</span>
+                    <span><span class="codeTable_linesCounter">6</span> lines </span>
+                </div>
+                <div class="codeTable__code">
+                    <table class="codeTable__table">
+                        <tr class="codeTable_stable">
+                            <th class="codeTable__numLine">1</th>
+                            <th class="codeTable__content">Hello world</th>
+                        </tr>
+                        <tr class="codeTable_stable">
+                            <th class="codeTable__numLine">2</th>
+                            <th class="codeTable__content">This is test message This is test message This is test message This is test messageThis is test message This is test message This is test message This is test message</th>
+                        </tr>
+                        <tr class="codeTable_stable">
+                            <th class="codeTable__numLine">3</th>
+                            <th class="codeTable__content">My name daniel</th>
+                        </tr>
+                        <tr class="codeTable_add">
+                            <th class="codeTable__numLine">4</th>
+                            <th class="codeTable__content">Hello world</th>
+                        </tr>
+                        <tr class="codeTable_delete">
+                            <th class="codeTable__numLine">5</th>
+                            <th class="codeTable__content">This is test message</th>
+                        </tr>
+                        <tr class="codeTable_edited">
+                            <th class="codeTable__numLine">6</th>
+                            <th class="codeTable__content">My name daniel</th>
+                        </tr>
+                        <tr class="codeTable_add">
+                            <th class="codeTable__numLine">4</th>
+                            <th class="codeTable__content">Hello world</th>
+                        </tr>
+                        <tr class="codeTable_delete">
+                            <th class="codeTable__numLine">5</th>
+                            <th class="codeTable__content">This is test message</th>
+                        </tr>
+                        <tr class="codeTable_edited">
+                            <th class="codeTable__numLine">6</th>
+                            <th class="codeTable__content">My name daniel</th>
+                        </tr>
+                    </table>
+                </div>
+            </div>
+
+            <div class="codeTable">
+                <div class="codeTable__head">
+                    <span>main.txt</span>
+                    <span><span class="codeTable_linesCounter">6</span> lines </span>
+                </div>
+                <div class="codeTable__code">
+                    <table class="codeTable__table">
+                        <tr class="codeTable_stable">
+                            <th class="codeTable__numLine">1</th>
+                            <th class="codeTable__content">Hello world</th>
+                        </tr>
+                        <tr class="codeTable_stable">
+                            <th class="codeTable__numLine">2</th>
+                            <th class="codeTable__content">This is test message This is test message This is test message This is test messageThis is test message This is test message This is test message This is test message</th>
+                        </tr>
+                        <tr class="codeTable_stable">
+                            <th class="codeTable__numLine">3</th>
+                            <th class="codeTable__content">My name daniel</th>
+                        </tr>
+                        <tr class="codeTable_add">
+                            <th class="codeTable__numLine">4</th>
+                            <th class="codeTable__content">Hello world</th>
+                        </tr>
+                        <tr class="codeTable_delete">
+                            <th class="codeTable__numLine">5</th>
+                            <th class="codeTable__content">This is test message</th>
+                        </tr>
+                        <tr class="codeTable_edited">
+                            <th class="codeTable__numLine">6</th>
+                            <th class="codeTable__content">My name daniel</th>
+                        </tr>
+                    </table>
+                </div>
+            </div>
+            <!-- /compRes__tables -->
+        </div>
+
+    </div>
+
+<?php
+require_once (__DIR__ . '/bottom.php');
